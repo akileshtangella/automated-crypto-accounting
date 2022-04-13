c39d32c5e7ca49c7e8ab1f8ecf851a2751c3f6cb
Akilesh Tangella <saiakilesh@gmail.com>
2022-04-13 05:52:49 -0400

    code is working

--- contracts/Accounting.sol
+++ contracts/Accounting.sol
@@ -1,1 +1,274 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+ 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract Accounting {
+
+    mapping (string => int256) public balanceSheet;
+    mapping (string => int256) public incomeStatement;
+    mapping (string => int256) public cashFlowStatement;
+
+    // Set of addresses that can conduct business on behalf of the company
+    mapping (address => bool) private admins;
+
+    // usdc Contract
+    address public usdc;
+
+    // The constructor initializes the admins mapping
+    constructor (address _usdc, address[] memory _adminArray) {
+        for(uint i = 0; i < _adminArray.length; i++) {
+            admins[_adminArray[i]] = true;
+        }
+
+        usdc = _usdc;
+    }
+
+    // Function modifier which checks whether the caller of a function is part of the admin set
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "Caller not part of the admin set");
+        _;
+    }
+
+    /**
+        This function is used to receive equity funds
+        @param shareholder the shareholder that is providing equity
+        @param equityAmount the amounty of equity funds being provided
+     */
+    function raiseEquity(address shareholder, int256 equityAmount) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] += equityAmount;
+        balanceSheet["CapitalStock"] += equityAmount;
+        calculateBalanceSheetTotals();
+
+        // No Updates to Income Statement
+        // Updates to Cash Flow Statement
+        cashFlowStatement["SaleOfCapitalStock"] += equityAmount;
+        calculateCashFlowStatementTotals();
+
+        // Receive the funds 
+        IERC20(usdc).transferFrom(shareholder, address(this), uint256(equityAmount));
+
+    }
+
+    /**
+     */
+    function paySalary(address employee, int256 salary) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] -= salary;
+        balanceSheet["RetainedEarnings"] -= salary;
+        calculateBalanceSheetTotals();
+
+        // Updates to Income Statement
+        incomeStatement["GeneralAndAdministrativeExpenses"] += salary;
+        calculateIncomeStatementTotals();
+
+        // Updates to Cash Flow Statement
+        cashFlowStatement["CashDisbursements"] += salary;
+        calculateCashFlowStatementTotals();
+
+        // Pay the employee
+        IERC20(usdc).transfer(employee, uint256(salary));
+    }
+
+    /**
+     */
+    function takeLongTermDebt(address loaner, int256 loanAmount, int8 interestRate) external onlyAdmin {
+        require(interestRate <= 100, "Not a valid interest rate");
+
+        int256 currentPortionOfDebt = loanAmount * interestRate / 100;
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] += loanAmount;
+        balanceSheet["LongTermDebt"] += loanAmount - currentPortionOfDebt;
+        balanceSheet["CurrentPortionOfDebt"] += currentPortionOfDebt;
+        calculateBalanceSheetTotals();
+
+        // No Updates to Income Statement
+        // Updates to Cash Flow Statement
+        cashFlowStatement["NetBorrowings"] += loanAmount;
+        calculateCashFlowStatementTotals();
+
+        // Receive Funds from Loaner
+        IERC20(usdc).transferFrom(loaner, address(this), uint256(loanAmount));
+    }
+
+    /**
+     */
+    function payInterestOnLongTermDebt(address loaner, int256 interestAmount) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] -= interestAmount;
+        balanceSheet["RetainedEarnings"] -= interestAmount;
+        calculateBalanceSheetTotals();
+
+        // Updates to Income Statement
+        incomeStatement["NetInterestIncome"] -= interestAmount;
+        calculateIncomeStatementTotals();
+
+        // Updates to Cash Flow Statement
+        cashFlowStatement["CashDisbursements"] += interestAmount;
+        calculateCashFlowStatementTotals();
+
+        // Pay Loaner
+        IERC20(usdc).transfer(loaner, uint256(interestAmount));
+    }
+
+
+    /**
+     */
+    function buyFixedAsset(address recipient, int256 fixedAssetCost, int256 amountToPayNow) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["FixedAssetsAtCost"] += fixedAssetCost;
+        balanceSheet["Cash"] -= amountToPayNow;
+        balanceSheet["AccountsPayable"] += fixedAssetCost - amountToPayNow;
+        calculateBalanceSheetTotals();
+
+        // No Updates to Income Statement
+        // Updates to Cash Flow Statement
+        cashFlowStatement["PPAndEPurchase"] += amountToPayNow;
+        calculateCashFlowStatementTotals();
+
+        // Make Payment for the Fixed Asset
+        IERC20(usdc).transfer(recipient, uint256(fixedAssetCost));
+    }
+
+    /**
+     */
+    function depreciateFixedAsset(int256 depreciation) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["AccumulatedDepreciation"] += depreciation;
+        balanceSheet["Inventories"] += depreciation;
+        calculateBalanceSheetTotals();
+
+        // No updates to Income Statement
+        // No updates to Cash Flow Statement
+    }
+
+    /**
+     */
+    function buyInventory(address seller, int256 cost, int256 amountToPayNow) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] -= amountToPayNow;
+        balanceSheet["Inventories"] += cost;
+        balanceSheet["AccountsPayable"] += cost - amountToPayNow;
+        calculateBalanceSheetTotals();
+
+        // No updates to Income Statement
+        // Updates to Cash Flow Statement
+        cashFlowStatement["CashDisbursements"] += amountToPayNow;
+        calculateCashFlowStatementTotals();
+
+        // Pay the seller
+        IERC20(usdc).transfer(seller, uint256(amountToPayNow));
+    }
+
+    /**
+     */
+    function sellInventory(address buyer, int256 revenueFromSale, int256 amountToReceiveNow, int256 costOfGoodsSold) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] += amountToReceiveNow;
+        balanceSheet["AccountsReceivable"] += revenueFromSale - amountToReceiveNow;
+        balanceSheet["Inventories"] -= costOfGoodsSold;
+        balanceSheet["RetainedEarnings"] += revenueFromSale - costOfGoodsSold;
+        calculateBalanceSheetTotals();
+
+        // Updates to Income Statement
+        incomeStatement["NetSales"
+        ] += revenueFromSale;
+        incomeStatement["CostOfGoodsSold"] += costOfGoodsSold;
+        calculateIncomeStatementTotals();
+
+        // Updates to Cash Flow Statement
+        cashFlowStatement["CashReceipts"] += amountToReceiveNow;
+        calculateCashFlowStatementTotals();
+
+        // Receive Funds from Buyer
+        IERC20(usdc).transferFrom(buyer, address(this), uint256(amountToReceiveNow));
+    }
+
+    /**
+     */
+    function payAccountsPayable(address owed, int256 amountToPay) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] -= amountToPay;
+        balanceSheet["AccountsPayable"] -= amountToPay;
+        calculateBalanceSheetTotals();
+
+        // No updates to Income Statement
+        // Updates to Cash Flow Statement
+        cashFlowStatement["CashDisbursements"] += amountToPay;
+        calculateCashFlowStatementTotals();
+
+        // Pay the owed party
+        IERC20(usdc).transfer(owed, uint256(amountToPay));
+    }
+
+    /**
+     */
+    function receiveAccountsReceivable(address ower, int256 amountToReceive) external onlyAdmin {
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] += amountToReceive;
+        balanceSheet["AccountsReceivable"] -= amountToReceive;
+        calculateBalanceSheetTotals();
+        
+        // No Updates to Income Statement
+        // Updates to Cash Flow Statement
+        cashFlowStatement["CashReceipts"] += amountToReceive;
+        calculateCashFlowStatementTotals();
+
+        // Receive cash from the ower
+        IERC20(usdc).transferFrom(ower, address(this), uint256(amountToReceive));
+    }
+
+    /**
+     */
+    function payTaxes(address government, int8 taxRate) external onlyAdmin {
+        require(taxRate <= 100, "Not a valid tax rate");
+        int256 taxAmount = incomeStatement["IncomeFromOperations"] * taxRate / 100;
+
+        // Updates to Balance Sheet
+        balanceSheet["Cash"] -= taxAmount;
+        balanceSheet["RetainedEarnings"] -= taxAmount;
+        calculateBalanceSheetTotals();
+
+        // Updates to Income Statement
+        incomeStatement["IncomeTaxes"] += taxAmount;
+        calculateIncomeStatementTotals();
+
+        // Updates to Cash Flow Statement
+        cashFlowStatement["IncomeTaxesPaid"] += taxAmount;
+        calculateCashFlowStatementTotals();
+
+        // Pay Taxes
+        IERC20(usdc).transfer(government, uint256(taxAmount));
+    }
+
+    function calculateBalanceSheetTotals() public {
+        balanceSheet["CurrentAssets"] = balanceSheet["Cash"] + balanceSheet["AccountsReceivable"] + balanceSheet["Inventories"];
+
+        balanceSheet["NetFixedAssets"] = balanceSheet["FixedAssetsAtCost"] - balanceSheet["AccumulatedDepreciation"];
+
+        balanceSheet["TotalAssets"] = balanceSheet["CurrentAssets"] + balanceSheet["NetFixedAssets"];
+
+        balanceSheet["CurrentLiabilities"] = balanceSheet["AccountsPayable"] + balanceSheet["CurrentPortionOfDebt"];
+
+        balanceSheet["ShareholdersEquity"] = balanceSheet["CapitalStock"] + balanceSheet["RetainedEarnings"];
+
+        balanceSheet["TotalLiabilitiesAndEquity"] = balanceSheet["CurrentLiabilities"] + balanceSheet["LongTermDebt"] + balanceSheet["ShareholdersEquity"];
+    }
+
+    function calculateIncomeStatementTotals() public {
+         incomeStatement["GrossMargin"] = incomeStatement["NetSales"] - incomeStatement["CostOfGoodsSold"];
+
+        incomeStatement["OperatingExpenses"] = incomeStatement["GeneralAndAdministrativeExpenses"];
+
+        incomeStatement["IncomeFromOperations"] = incomeStatement["GrossMargin"] - incomeStatement["OperatingExpenses"];
+
+        incomeStatement["NetIncome"] = incomeStatement["IncomeFromOperations"] + incomeStatement["NetInterestIncome"] - incomeStatement["IncomeTaxes"];
+    }
+
+    function calculateCashFlowStatementTotals() public {
+        cashFlowStatement["CashFlowFromOperations"] = cashFlowStatement["CashReceipts"] - cashFlowStatement["CashDisbursements"];
+
+        cashFlowStatement["EndingCashBalance"] = cashFlowStatement["BeginningCashBalance"] + cashFlowStatement["CashFlowFromOperations"] - cashFlowStatement["PPAndEPurchase"] + cashFlowStatement["NetBorrowings"] - cashFlowStatement["IncomeTaxesPaid"] + cashFlowStatement["SaleOfCapitalStock"];
+    }
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,22 +4,142 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { Accounting__factory, USDC__factory } from "../typechain";
+import Table = require("cli-table");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const signers = await ethers.getSigners();
+  const stacy = signers[0];
+  const shareholder = signers[1];
+  const loaner = signers[2];
+  const government = signers[3];
+  const jugSeller = signers[4];
+  const rawMaterialsSeller = signers[5];
+  const alice = signers[6];
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // Deploy USDC Contract
+  const USDCFactory = new USDC__factory(stacy);
+  const USDC = await USDCFactory.deploy();
+  await USDC.deployed();
 
-  await greeter.deployed();
+  // Mint USDC to the various parties
+  await USDC.mint(shareholder.address, 5000000);
+  await USDC.mint(loaner.address, 5000000);
+  await USDC.mint(alice.address, 5000000);
 
-  console.log("Greeter deployed to:", greeter.address);
+  async function printFinancialStatements() {
+    const balanceSheetTable = new Table();
+    const incomeStatementTable = new Table();
+    const cashFlowStatementTable = new Table();
+
+    balanceSheetTable.push(
+      {'ASSETS': ''}, 
+      {'(1) Cash': await accounting.balanceSheet('Cash')},
+      {'(2) Accounts Receivable':  await accounting.balanceSheet('AccountsReceivable')},
+      {'(3) Inventories': await accounting.balanceSheet('Inventories')},
+      {'(4) Current Assets = (1)+(2)+(3)': await accounting.balanceSheet('CurrentAssets')},
+      {'(5) Fixed Assets at Cost': await accounting.balanceSheet('FixedAssetsAtCost')},
+      {'(6) Accumulated Depreciation': await accounting.balanceSheet('AccumulatedDepreciation')},
+      {'(7) Net Fixed Assets = (5)-(6)': await accounting.balanceSheet('NetFixedAssets')},
+      {'(8) Total Assets = (4)+(7)': await accounting.balanceSheet('TotalAssets')},
+      {'': ''}, 
+      {'LIABILITIES AND EQUITY': ''}, 
+      {'(9) Accounts Payable': await accounting.balanceSheet('AccountsPayable')},
+      {'(10) Current Portion of Debt': await accounting.balanceSheet('CurrentPortionOfDebt')},
+      {'(11) Current Liabilities = (9)+(10)': await accounting.balanceSheet('CurrentLiabilities')},
+      {'(12) Long-Term Debt': await accounting.balanceSheet('LongTermDebt')},
+      {'(13) Capital Stock': await accounting.balanceSheet('CapitalStock')},
+      {'(14) Retained Earnings': await accounting.balanceSheet('RetainedEarnings')},
+      {'(15) Shareholders\' Equity = (13)-(14)': await accounting.balanceSheet('ShareholdersEquity')},
+      {'(16) Total Liabilities and Equity = (11)+(12)+(15)': await accounting.balanceSheet('TotalLiabilitiesAndEquity')}
+    );
+
+    console.log(balanceSheetTable.toString());
+
+    incomeStatementTable.push(
+      {'INCOME STATEMENT': ''}, 
+      {'(1) Net Sales': await accounting.incomeStatement('NetSales')},
+      {'(2) Cost of Goods Sold': await accounting.incomeStatement('CostOfGoodsSold')},
+      {'(3) Gross Margin = (1)-(2)': await accounting.incomeStatement('GrossMargin')},
+      {'(4) General and Administrative Expenses': await accounting.incomeStatement('GeneralAndAdministrativeExpenses')},
+      {'(5) Operating Expenses = (4)': await accounting.incomeStatement('OperatingExpenses')},
+      {'(6) Income from Operations = (3)-(5)': await accounting.incomeStatement('IncomeFromOperations')},
+      {'(7) Net Interest Income': await accounting.incomeStatement('NetInterestIncome')},
+      {'(8) Income Taxes': await accounting.incomeStatement('IncomeTaxes')},
+      {'(9) Net Income=(6)+(7)-(8)': await accounting.incomeStatement('NetIncome')},
+    );
+
+    console.log(incomeStatementTable.toString());
+
+    cashFlowStatementTable.push(
+      {'CASH FLOW STATEMENT': ''},
+      {'(1) Beginning Cash Balance': await accounting.cashFlowStatement('BeginningCashBalance')},
+      {'(2) Cash Receipts': await accounting.cashFlowStatement('CashReceipts')},
+      {'(3) Cash Disbursements': await accounting.cashFlowStatement('CashDisbursements')},
+      {'(4) Cash Flow from Operations = (2)-(3)': await accounting.cashFlowStatement('CashFlowFromOperations')},
+      {'(5) PP&E Purchase': await accounting.cashFlowStatement('PPAndEPurchase')},
+      {'(6) Net Borrowings': await accounting.cashFlowStatement('NetBorrowings')},
+      {'(7) Income Taxes Paid': await accounting.cashFlowStatement('IncomeTaxesPaid')},
+      {'(8) Sale of Capital Stock': await accounting.cashFlowStatement('SaleOfCapitalStock')},
+      {'(9) Ending Cash Balance = (4)-(5)+(6)-(7)+(8)': await accounting.cashFlowStatement('EndingCashBalance')}
+    )
+
+    console.log(cashFlowStatementTable.toString());
+  }
+
+  console.log('Stacy Deploys the Accounting Contract');
+  const accountingFactory = new Accounting__factory(stacy);
+  const accounting = await accountingFactory.deploy(USDC.address, [stacy.address]);
+  await accounting.deployed();
+  await printFinancialStatements();
+
+  console.log('Stacy Raises $1,000,000 of Equity from Investors');
+  await USDC.connect(shareholder).approve(accounting.address, 1000000);
+  await accounting.raiseEquity(shareholder.address, 1000000);
+  await printFinancialStatements();
+
+  console.log('Stacy Pays Herself $5000 in Salary');
+  await accounting.paySalary(stacy.address, 5000);
+  await printFinancialStatements();
+
+  console.log('Stacy Takes a 10 Year Loan and Buys a Plot of Land for the Lemonade Stand for $50,000 at a 10% Yearly Interest Rate');
+  await USDC.connect(loaner).approve(accounting.address, 50000)
+  await accounting.takeLongTermDebt(loaner.address, 50000, 10);
+  await printFinancialStatements();
+
+  console.log('Stacy Buys a Jug to Make Her Lemonade In. The Total Cost is $10,000, but She Only Pays for Half Now');
+  await accounting.buyFixedAsset(jugSeller.address, 10000, 5000);
+  await printFinancialStatements();
+
+  console.log('Stacy Buys Lemonade Powder and Water, the Raw Materials Needed to Make Lemonade for $500');
+  await accounting.buyInventory(rawMaterialsSeller.address, 500, 500);
+  await printFinancialStatements();
+
+  console.log('Stacy Makes Lemonade and Sells All of it to Alice $20,000. She Receives Cash for Half of the Lemonade');
+  await USDC.connect(alice).approve(accounting.address, 10000);
+  await accounting.sellInventory(alice.address, 20000, 10000, 500);
+  await printFinancialStatements();
+
+  console.log('Stacy Pays Off the Other $5,000 She Owes for the Jug');
+  await accounting.payAccountsPayable(jugSeller.address, 5000);
+  await printFinancialStatements();
+
+  console.log('Stacy Receives the Other $10,000 She is Owed From Alice');
+  await USDC.connect(alice).approve(accounting.address, 10000);
+  await accounting.receiveAccountsReceivable(alice.address, 10000);
+  await printFinancialStatements();
+
+  console.log('It\'s the End of the Fiscal Year. Stacy Pays the Interest She Owes on the Loan She Used to Buy the Plot of Land');
+  await accounting.payInterestOnLongTermDebt(loaner.address, 5000);
+  await printFinancialStatements();
+
+  console.log('Stacy Accumulates Depreciation on Her Jug. She Uses Straight Line Depreciation Over 5 Years');
+  await accounting.depreciateFixedAsset(2000);
+  await printFinancialStatements();
+
+  console.log('Stacy Pays Income Taxes at a Tax Rate of 30%');
+  await accounting.payTaxes(government.address, 30);
+  await printFinancialStatements();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
